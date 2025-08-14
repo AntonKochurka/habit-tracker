b948ce268efb49b6316be3eb9e8b463c6c3cea9a
AntonKochurka <antonkotchurka@gmail.com>
2025-08-14 21:11:21 +0300

    Add: alies and pathes for frontend to make code look better.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './apps/app.tsx'
+
+import App from '@app/app'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  root: __dirname,
+  envDir: path.resolve(__dirname, ".."),
+  cacheDir: "./node_modules/.vite/.",
+
+
+  server: {
+    host: "localhost",
+    port: 4200
+  },
+
+  resolve: { 
+    alias: {
+      "@app": path.resolve(__dirname, "src", "apps"),
+      "@shared": path.resolve(__dirname, "src", "shared"),
+      "@components": path.resolve(__dirname, "src", "shared", "components")
+    }
+  }
 })
